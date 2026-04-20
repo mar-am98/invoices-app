29fe83bbe9f260f1bda027dc67f9b8ba636a8cf3
Maram Emad <maramemad674@gmail.com>
2026-04-21 00:20:50 +0300

    chore: initial project setup with vite, tailwindcss, and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-white bg-red-500">Hello react</h1>
+      <Button className='my-5'>
+        Button
+      </Button>
+    </>
+  )
+}
+
+export default App
