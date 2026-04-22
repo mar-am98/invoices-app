dd8e48f1e48702a6f1a191503b91d21c09f02e74
Maram Emad <maramemad674@gmail.com>
2026-04-22 15:41:19 +0300

    feat: setup ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
 import { SideNavbar } from "./components/global/SideNavbar";
 import Filter from "./components/global/Filter";
 import AddNewBtn from "./components/global/AddNewBtn";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <div className="flex flex-col lg:flex-row min-h-screen bg-background">
-      <SideNavbar />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="flex flex-col lg:flex-row min-h-screen bg-background">
+        <SideNavbar />
 
-      <main className="flex-1 flex justify-center py-8 px-6 lg:py-16">
-        <div className="w-full max-w-183 mx-auto">
-          <header className="flex justify-between items-center mb-10 lg:mb-16">
-            <div>
-              <h1 className="text-2xl lg:text-3xl font-bold tracking-tight text-sidebar-foreground">
-                Invoices
-              </h1>
-              <p className="text-chart-4 text-sm">
-                <span className="hidden md:inline">There are 7 total</span> 7 invoices
-              </p>
-            </div>
+        <main className="flex-1 flex justify-center py-8 px-6 lg:py-16">
+          <div className="w-full max-w-183 mx-auto">
+            <header className="flex justify-between items-center mb-10 lg:mb-16">
+              <div>
+                <h1 className="text-2xl lg:text-3xl font-bold tracking-tight text-sidebar-foreground">
+                  Invoices
+                </h1>
+                <p className="text-chart-4 text-sm">
+                  <span className="hidden md:inline">There are 7 total</span> 7 invoices
+                </p>
+              </div>
 
-            <div className="flex items-center gap-4 lg:gap-10">
-              <Filter />
-              <AddNewBtn />
-            </div>
-          </header>
-        </div>
-      </main>
-    </div>
+              <div className="flex items-center gap-4 lg:gap-10">
+                <Filter />
+                <AddNewBtn />
+              </div>
+            </header>
+          </div>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
