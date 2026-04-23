e5fd58c7d38bae850efdf9a4d320be48dfd640e3
Maram Emad <maramemad674@gmail.com>
2026-04-23 22:29:36 +0300

    refactor: update filter section and add dropdown

--- src/components/global/Filter.tsx
+++ src/components/global/Filter.tsx
@@ -1,12 +1,27 @@
 import { ChevronDown } from "lucide-react"
 import { Button } from "../ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
 
 function Filter() {
   return (
-    <Button variant={'ghost'} className="font-bold text-sm cursor-pointer flex items-center gap-2">
-        Filter <span className="hidden md:inline">by status</span>
-        <span className="text-primary"><ChevronDown /></span>
-    </Button>
+    
+    <>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={'ghost'} className="font-bold text-sm cursor-pointer flex items-center gap-2">
+            Filter <span className="hidden md:inline">by status</span>
+            <span className="text-primary"><ChevronDown /></span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>Draft</DropdownMenuItem>
+          <DropdownMenuItem>Pending</DropdownMenuItem>
+          <DropdownMenuItem>Paid</DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+    </>
   )
 }
 
