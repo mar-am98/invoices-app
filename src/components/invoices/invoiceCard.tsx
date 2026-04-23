a9b00ea81db55b0266e4461de98a3bbb885aa19c
Maram Emad <maramemad674@gmail.com>
2026-04-23 15:40:50 +0300

    refactor: change the statusBadge to component

--- src/components/invoices/invoiceCard.tsx
+++ src/components/invoices/invoiceCard.tsx
@@ -1,6 +1,6 @@
 import { ChevronRight } from "lucide-react"
 import { Link } from "react-router-dom"
-import { Badge } from "@/components/ui/badge"
+import StatusBadge from "./StatusBadge";
 
 interface InvoiceProps {
   id: string;
@@ -11,15 +11,6 @@ interface InvoiceProps {
 }
 
 export default function InvoiceCard({ id, dueDate, name, total, status }: InvoiceProps) {
-  const colors = {
-    paid: 'bg-[#33D69F]/5 text-[#33D69F]',
-    pending: 'bg-[#FF8F00]/5 text-[#FF8F00]',
-    draft: 'bg-secondary-foreground/5 text-secondary-foreground'
-  }
-  const color = colors[status] || colors.draft;
-
-
-
   return (
     <Link to={`/invoice/${id}`}>
       <div className="flex items-center justify-between bg-card p-6 rounded-lg shadow-sm border border-transparent hover:border-purple-500 transition-all mb-4 max-sm:flex-col max-sm:gap-1">
@@ -35,10 +26,7 @@ export default function InvoiceCard({ id, dueDate, name, total, status }: Invoic
         <div className="flex items-center gap-8 max-sm:w-full max-sm:justify-between">
           <span className="font-bold text-lg">£{total.toLocaleString()}</span>
           
-          <Badge className={`w-26 h-10 flex items-center justify-center gap-2 py-2 capitalize font-bold border-none shadow-none rounded-sm ${color} max-sm:-translate-y-2`}>
-             <span className="size-2 rounded-full bg-current" />
-             {status}
-          </Badge>
+          <StatusBadge status={status}/>
 
           <ChevronRight className="text-primary size-5 hidden md:block" />
         </div>
